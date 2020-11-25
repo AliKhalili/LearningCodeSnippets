5067605438fe819c6d218618bf0071906e6a80f4
ali <akyegane@gmail.com>
2020-11-25 10:40:34 +0330

    add  available methods in DEBUG model

--- microservice/blob_storage_fastapi/config.py
+++ microservice/blob_storage_fastapi/config.py
@@ -1,1 +1,5 @@
+import logging
+
 SQLALCHEMY_DATABASE_URI = "sqlite:///D:\\_temp_python\\foo.db"
+
+LOG_LEVEL = logging.DEBUG
--- microservice/blob_storage_fastapi/main.py
+++ microservice/blob_storage_fastapi/main.py
@@ -1,3 +1,5 @@
+from tabulate import tabulate
+
 import logging
 
 from fastapi import FastAPI, Depends
@@ -5,10 +7,11 @@ import uvicorn
 from starlette.applications import Starlette
 
 from api import api_router
-from data.database import get_db
+from logger import configure_logging
 from middleware import SetDbMiddleware
 
 log = logging.getLogger(__name__)
+configure_logging()
 
 app = Starlette()
 api = FastAPI()
@@ -18,6 +21,11 @@ api.include_router(api_router, prefix="/v1")
 
 app.mount("/api", app=api)
 
+table = []
+for r in api_router.routes:
+    table.append([r.path, ",".join(r.methods)])
+
+log.debug("Available Endpoints \n" + tabulate(table, headers=["Path", "Methods"]))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
