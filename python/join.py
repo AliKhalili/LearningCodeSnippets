71d50ec6383970ad88ccdd614b8b8694659c544c
ali <akyegane@gmail.com>
2020-12-09 10:03:29 +0330

    performance comparison between built-in join and reduce function in python

---
+++ python/join.py
@@ -0,0 +1,29 @@
+import array
+import time
+from functools import reduce
+import dis
+
+
+def builtin_join(arr: array):
+    return ''.join(arr)
+
+
+def reduce_join(arr: array):
+    return reduce(lambda c, n: f'{c},{n}', arr)
+
+
+foo_list = array.array('u', ['a'] * 100_000)
+
+t_start = time.time()
+join_result = builtin_join(foo_list)
+t_end = time.time()
+print('---join---')
+dis.dis(builtin_join)
+print(f'join -> {(t_end - t_start):8f}')
+
+t_start = time.time()
+reduce_result = reduce_join(foo_list)
+t_end = time.time()
+print('---reduce---')
+dis.dis(reduce_join)
+print(f'reduce -> {(t_end - t_start):8f}')
